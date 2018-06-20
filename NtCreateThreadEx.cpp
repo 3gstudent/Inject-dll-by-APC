250c4783436b025f84710fe092ed60662168eab3
3gstudent <robertpetersen@outlook.com>
2018-06-20 01:05:16 -0700

    Update and rename CreateRemoteThread.cpp to NtCreateThreadEx.cpp

---
+++ NtCreateThreadEx.cpp
@@ -0,0 +1,196 @@
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <tlhelp32.h>
+#pragma comment(lib,"Advapi32.lib") 
+
+typedef NTSTATUS(NTAPI* pfnNtCreateThreadEx)
+(
+	OUT PHANDLE hThread,
+	IN ACCESS_MASK DesiredAccess,
+	IN PVOID ObjectAttributes,
+	IN HANDLE ProcessHandle,
+	IN PVOID lpStartAddress,
+	IN PVOID lpParameter,
+	IN ULONG Flags,
+	IN SIZE_T StackZeroBits,
+	IN SIZE_T SizeOfStackCommit,
+	IN SIZE_T SizeOfStackReserve,
+	OUT PVOID lpBytesBuffer);
+
+#define NT_SUCCESS(x) ((x) >= 0)
+
+typedef struct _CLIENT_ID {
+	HANDLE UniqueProcess;
+	HANDLE UniqueThread;
+} CLIENT_ID, *PCLIENT_ID;
+
+typedef NTSTATUS(NTAPI * pfnRtlCreateUserThread)(
+	IN HANDLE ProcessHandle,
+	IN PSECURITY_DESCRIPTOR SecurityDescriptor OPTIONAL,
+	IN BOOLEAN CreateSuspended,
+	IN ULONG StackZeroBits OPTIONAL,
+	IN SIZE_T StackReserve OPTIONAL,
+	IN SIZE_T StackCommit OPTIONAL,
+	IN PTHREAD_START_ROUTINE StartAddress,
+	IN PVOID Parameter OPTIONAL,
+	OUT PHANDLE ThreadHandle OPTIONAL,
+	OUT PCLIENT_ID ClientId OPTIONAL);
+
+BOOL InjectDll(UINT32 ProcessId, char *DllFullPath)
+{
+	HANDLE ProcessHandle = NULL;
+
+	ProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessId);
+	if (ProcessHandle == NULL)
+	{
+		printf("[!]OpenProcess error\n");
+		return FALSE;
+	}
+
+	UINT32    DllFullPathLength = (strlen(DllFullPath) + 1);
+	PVOID     DllFullPathBufferData = VirtualAllocEx(ProcessHandle, NULL, DllFullPathLength, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	if (DllFullPathBufferData == NULL)
+	{
+		CloseHandle(ProcessHandle);
+		printf("[!]DllFullPathBufferData error\n");
+		return FALSE;
+	}
+	SIZE_T    ReturnLength;
+	BOOL bOk = WriteProcessMemory(ProcessHandle, DllFullPathBufferData, DllFullPath, strlen(DllFullPath) + 1, &ReturnLength);
+
+	LPTHREAD_START_ROUTINE    LoadLibraryAddress = NULL;
+	HMODULE                    Kernel32Module = GetModuleHandle("Kernel32");
+	LoadLibraryAddress = (LPTHREAD_START_ROUTINE)GetProcAddress(Kernel32Module, "LoadLibraryA");
+	pfnNtCreateThreadEx NtCreateThreadEx = (pfnNtCreateThreadEx)GetProcAddress(GetModuleHandle("ntdll.dll"), "NtCreateThreadEx");
+	if (NtCreateThreadEx == NULL)
+	{
+		CloseHandle(ProcessHandle);
+		printf("[!]NtCreateThreadEx error\n");
+		return FALSE;
+	}
+	HANDLE ThreadHandle = NULL;
+	NtCreateThreadEx(&ThreadHandle, 0x1FFFFF, NULL, ProcessHandle, (LPTHREAD_START_ROUTINE)LoadLibraryAddress, DllFullPathBufferData, FALSE, NULL, NULL, NULL, NULL);
+	if (ThreadHandle == NULL)
+	{
+		CloseHandle(ProcessHandle);
+		printf("[!]ThreadHandle error\n");
+		return FALSE;
+	}
+	if (WaitForSingleObject(ThreadHandle, INFINITE) == WAIT_FAILED)
+	{
+		printf("[!]WaitForSingleObject error\n");
+		return FALSE;
+	}
+	CloseHandle(ProcessHandle);
+	CloseHandle(ThreadHandle);
+	return TRUE;
+}
+
+
+BOOL FreeDll(UINT32 ProcessId, char *DllFullPath)
+{
+	BOOL bMore = FALSE, bFound = FALSE;
+	HANDLE hSnapshot;
+	HMODULE hModule = NULL;
+	MODULEENTRY32 me = { sizeof(me) };
+	BOOL bSuccess = FALSE;
+	hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, ProcessId);
+	bMore = Module32First(hSnapshot, &me);
+	for (; bMore; bMore = Module32Next(hSnapshot, &me)) {
+		if (!_tcsicmp((LPCTSTR)me.szModule, DllFullPath) ||
+			!_tcsicmp((LPCTSTR)me.szExePath, DllFullPath)) {
+			bFound = TRUE;
+			break;
+		}
+	}
+	if (!bFound) {
+		CloseHandle(hSnapshot);
+		return FALSE;
+	}
+	
+	HANDLE ProcessHandle = NULL;
+
+	ProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessId);
+
+	if (ProcessHandle == NULL)
+	{
+		printf("[!]OpenProcess error\n");
+		return FALSE;
+	}
+
+	LPTHREAD_START_ROUTINE    FreeLibraryAddress = NULL;
+	HMODULE                    Kernel32Module = GetModuleHandle("Kernel32");
+	FreeLibraryAddress = (LPTHREAD_START_ROUTINE)GetProcAddress(Kernel32Module, "FreeLibrary");
+	pfnNtCreateThreadEx NtCreateThreadEx = (pfnNtCreateThreadEx)GetProcAddress(GetModuleHandle("ntdll.dll"), "NtCreateThreadEx");
+	if (NtCreateThreadEx == NULL)
+	{
+		CloseHandle(ProcessHandle);
+		printf("[!]NtCreateThreadEx error\n");
+		return FALSE;
+	}
+	HANDLE ThreadHandle = NULL;
+
+	NtCreateThreadEx(&ThreadHandle, 0x1FFFFF, NULL, ProcessHandle, (LPTHREAD_START_ROUTINE)FreeLibraryAddress, me.modBaseAddr, FALSE, NULL, NULL, NULL, NULL);
+	if (ThreadHandle == NULL)
+	{
+		CloseHandle(ProcessHandle);
+		printf("[!]ThreadHandle error\n");
+		return FALSE;
+	}
+	if (WaitForSingleObject(ThreadHandle, INFINITE) == WAIT_FAILED)
+	{
+		printf("[!]WaitForSingleObject error\n");
+		return FALSE;
+	}
+	CloseHandle(ProcessHandle);
+	CloseHandle(ThreadHandle);
+	return TRUE;
+}
+
+BOOL EnableDebugPrivilege(BOOL fEnable)
+{
+	BOOL fOk = FALSE;
+	HANDLE hToken;
+	if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken))
+	{
+		TOKEN_PRIVILEGES tp;
+		tp.PrivilegeCount = 1;
+		LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &tp.Privileges[0].Luid);
+		tp.Privileges[0].Attributes = fEnable ? SE_PRIVILEGE_ENABLED : 0;
+		AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(tp), NULL, NULL);
+		fOk = (GetLastError() == ERROR_SUCCESS);
+		CloseHandle(hToken);
+	}
+	return(fOk);
+}
+
+int main(int argc, char *argv[])
+{
+	
+	if (!EnableDebugPrivilege(TRUE))
+	{
+		printf("[!]AdjustTokenPrivileges Failed.<%d>\n", GetLastError());
+	}
+
+	if (argc != 3)
+	{
+		printf("Usage:\n");
+		printf("%s <PID> <Dll Full Path>\n", argv[0]);
+		return 0;
+	}
+	if (!InjectDll((DWORD)atoi(argv[1]), argv[2]))
+	{
+		printf("[!]InjectDll error \n");
+		return 1;
+	}
+	printf("[+]InjectDll success\n");
+	
+	if (!FreeDll((DWORD)atoi(argv[1]), argv[2]))
+	{
+		printf("[!]FreeDll error \n");
+		return 1;
+	}
+	printf("[+]FreeDll success\n");
+	return 0;
+}
